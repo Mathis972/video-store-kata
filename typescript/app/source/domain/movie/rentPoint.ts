57c3f721a07ee9962a73868841d9cd07e7bfd1c6
Mathis <eyzex@hotmail.fr>
2023-06-20 15:34:20 +0200

    made MovieConfiguration class smaller

--- typescript/app/source/domain/movie/rentPoint.ts
+++ typescript/app/source/domain/movie/rentPoint.ts
@@ -1,17 +1,18 @@
-import {Rental} from "./videoStore";
+import { Rental } from './videoStore';
 
-const rentPointsFor =
-    (f:(r:Rental)=>number):
-        (rentals:Rental[])=>number=>{
-        return (rentals) => rentals.map(r=>f(r)).reduce((x,y)=>x+y);
-    };
-
-const rentPointFor = (r:Rental):number=>{
-    let baserenterPoint = 1;
-    if(r.rentalDays>1){
-        return baserenterPoint+ r.mc.additionalRenterPoint
-    }
-    return baserenterPoint
+const rentPointsFor = (
+  f: (r: Rental) => number
+): ((rentals: Rental[]) => number) => {
+  return (rentals) => rentals.map((r) => f(r)).reduce((x, y) => x + y);
 };
 
-export const calculateRentalPoints: (rentals: Rental[]) => number = rentPointsFor(rentPointFor)
+const rentPointFor = (r: Rental): number => {
+  let baserenterPoint = 1;
+  if (r.rentalDays > 1) {
+    return baserenterPoint + r.mc.rentInfo.additionalRenterPoint;
+  }
+  return baserenterPoint;
+};
+
+export const calculateRentalPoints: (rentals: Rental[]) => number =
+  rentPointsFor(rentPointFor);
--- typescript/app/source/domain/movie/videoStore.ts
+++ typescript/app/source/domain/movie/videoStore.ts
@@ -1,43 +1,53 @@
 export class MoviePrices {
-    additionalCost: number;
-    movieBasePrice: number;
+  additionalCost: number;
+  movieBasePrice: number;
 
-    constructor(additionalCost: number, movieBasePrice: number) {
-        this.additionalCost = additionalCost;
-        this.movieBasePrice = movieBasePrice;
-    }
+  constructor(additionalCost: number, movieBasePrice: number) {
+    this.additionalCost = additionalCost;
+    this.movieBasePrice = movieBasePrice;
+  }
 }
 export class Rental {
-    rentalDays: number;
-    mc: MovieConfiguration;
+  rentalDays: number;
+  mc: MovieConfiguration;
 
-    constructor(rentalDays: number, m: MovieConfiguration) {
-        this.rentalDays = rentalDays;
-        this.mc = m;
-    }
+  constructor(rentalDays: number, m: MovieConfiguration) {
+    this.rentalDays = rentalDays;
+    this.mc = m;
+  }
+}
+export class MovieRentInfo {
+  minRentDays: number;
+  additionaCostPerDay: number;
+  additionalRenterPoint: number;
+  constructor(
+    minRentDays: number,
+    additionaCostPerDay: number,
+    additionalRenterPoint: number
+  ) {
+    this.minRentDays = minRentDays;
+    this.additionaCostPerDay = additionaCostPerDay;
+    this.additionalRenterPoint = additionalRenterPoint;
+  }
 }
 export class MovieConfiguration {
-    title: string;
-    price: number;
-    minRentDays: number;
-    additionaCostPerDay: number;
-    additionalRenterPoint: number;
+  title: string;
+  price: number;
+  rentInfo: MovieRentInfo;
 
-    constructor(title: string,
-                price: number,
-                minRentDays: number,
-                additionaCostPerDay: number,
-                additionalRenterPoint: number) {
-        this.title = title;
-        this.price = price;
-        this.minRentDays = minRentDays;
-        this.additionaCostPerDay = additionaCostPerDay;
-        this.additionalRenterPoint = additionalRenterPoint;
-    }
+  constructor(title: string, price: number, rentInfo: MovieRentInfo) {
+    this.title = title;
+    this.price = price;
+    this.rentInfo = rentInfo;
+  }
 }
-export const newReleaseConfiguration: (title: string) => MovieConfiguration = (title:string)=>{
-    return  new MovieConfiguration(title,3.0,1,3.0,1)
+export const newReleaseConfiguration: (title: string) => MovieConfiguration = (
+  title: string
+) => {
+  return new MovieConfiguration(title, 3.0, new MovieRentInfo(1, 3.0, 1));
 };
-export const childrenConfiguration: (title: string) => MovieConfiguration = (title:string)=>{
-    return  new MovieConfiguration(title,1.5,3,1.5,0)
+export const childrenConfiguration: (title: string) => MovieConfiguration = (
+  title: string
+) => {
+  return new MovieConfiguration(title, 1.5, new MovieRentInfo(3, 1.5, 0));
 };
